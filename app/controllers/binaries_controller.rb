d66fc1681a9b433ff5cbbbbae2588b6dc0c6cd44
yukikato <0me3210g039530z@gmail.com>
2019-03-30 17:38:56 +0900

    二進数のコントローラなどを実装

--- app/controllers/binaries_controller.rb
+++ app/controllers/binaries_controller.rb
@@ -1,4 +1,23 @@
 class BinariesController < ApplicationController
   def question
+    @binary = Binary.new
+    @question = rand(10)
+    @binary.correct_answer = @question
+    @question = @question.to_s(2)
+  end
+
+  def answer
+    @binary = Binary.new(binary_params)
+    if @binary.answer == @binary.correct_answer
+      render :answer, notice: "正解"
+    else
+      render :answer, notice: "不正解"
+    end
+  end
+
+  private
+
+  def binary_params
+    params.require(:binary).permit(:answer, :correct_answer)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'binaries/question'
+  get '/binaries', to: 'binaries#question'
+  post '/binaries', to: 'binaries#answer'
+  get '/binaries/answer', to: 'binaries#answer'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
