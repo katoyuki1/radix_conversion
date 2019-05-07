8a87df7fd1ca3db15a6a6c12ebb46bfc9294618d
yukikato <0me3210g039530z@gmail.com>
2019-05-08 08:35:14 +0900

    トッページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'tops#index'
+
   get '/binaries', to: 'binaries#question'
   post '/binaries', to: 'binaries#answer'
   get '/binaries/answer', to: 'binaries#answer'
